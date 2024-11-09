9ebd0cf29923fc18c0a15ba2075879763e9e9ead
Bharat99978 <166504281+Bharat99978@users.noreply.github.com>
2024-11-09 10:59:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const axios = require('axios');
 
-const url = 'https://test1.telegrambot9oks.free.nf/p.php';
+const url = 'http://test1.telegrambot9oks.free.nf/p.php';
 
 // Function to visit the website
 const visitWebsite = async () => {
   try {
     const response = await axios.get(url);
     console.log(`Visited ${url} - Status: ${response.status}`);
+    console.log("Web Page Content:");
+    console.log(response.data); // Logs the HTML content of the page
   } catch (error) {
     console.error(`Error visiting ${url}:`, error.message);
   }
 };
 
-// Refresh every 40 seconds (40000 milliseconds)
-setInterval(visitWebsite, 10000);
+// Refresh every 10 seconds (20000 milliseconds)
+setInterval(visitWebsite, 20000);
